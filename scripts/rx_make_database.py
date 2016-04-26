86d44b00d085201042ad73c85298b4a5b054a3d1
Alex Nelson <alexander.nelson@nist.gov>
2016-04-26 11:05:11 -0400

    Change debug message to print object representation
    There was a problem I encountered using this list comprehension.
    Unfortunately, the encountered problem is lost in my notes, though the
    solution was to not try being fancy with formatting.
    
    Reviewed-by: Lee Badger <mark.badger@nist.gov>
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- scripts/rx_make_database.py
+++ scripts/rx_make_database.py
@@ -27,12 +27,25 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    
+#
+# For changes made after April 1, 2016:
+#
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
 
 """
 For usage, run without arguments.
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 import sys
 
@@ -402,7 +415,7 @@ def main():
         successful_regxmls[hive_path] = xml_path
     if args.verbose:
         print("Successful hive file-RegXML pairs:")
-        print("\n".join([(k,successful_regxmls[k]) for k in successful_regxmls]))
+        print(repr(successful_regxmls))
 
     #Produce a list of the images to use
     work_list_unordered = []
